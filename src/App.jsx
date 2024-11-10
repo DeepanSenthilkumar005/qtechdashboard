ff7da2f0413f5d945ea7e9a57914ba331b92a7a8
DeepanSenthilkumar005 <deepancse45@gmail.com>
2024-11-10 07:36:13 +0530

    feat:add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import SideBar from './Components/SideBar'
+import Dashboard1 from './Components/Dashboard1'
+
+function App() {
+  return (
+    < >
+    
+    <Dashboard1 />
+    </>
+  )
+}
+
+export default App
